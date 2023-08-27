539c5a501de1ad07194ab596e541ee409f83d108
VitorDuraes <vitordo09@hotmail.com>
2023-08-26 21:50:30 -0300

    Messagem de alert

--- public/script.js
+++ public/script.js
@@ -1,1 +1,11 @@
 // front-end
+const closeMessage = document.querySelector("#close-message");
+const message = document.querySelector(".message");
+
+closeMessage.addEventListener("click", () => {
+  message.style.display = "none";
+});
+
+setTimeout(() => {
+  message.style.display = "none";
+}, 2000);
